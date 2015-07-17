39f2366d42afb8c5be3036d129e4f364766405e0
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-16 19:08:15 -0700

    observe not sending request

--- js/app.js
+++ js/app.js
@@ -1,38 +1,9 @@
 var React = require('react');
 var Parse = require('parse').Parse;
 var ParseReact = require('parse-react');
-var $ = require('jQuery');
-
-// console.log($);
 
 Parse.initialize("SedOxWgWCarMJnHhZG4qPznAwkA9oCODrSWnR0mt", "h1Kz38TDuDMh093nB8WdWcu5wpdKoOEXl06PVEjF");
 
-
-for (var i = 0; i < 10; i++) {
-  var Comment = Parse.Object.extend("Comment");
-  var comment = new Comment();
-  comment.set("info", "item # "+i);
-
-  comment.save(null, {
-    success: function(object) {
-      console.log("success");
-    },
-
-    error: function(object, error) {
-      console.log("error")
-    }
-
-  })
-}
-
-
-
-
-
-
-
-
-// tutorial1.js
 var CommentBox = React.createClass({
   render: function() {
     return (
@@ -45,38 +16,18 @@ var CommentBox = React.createClass({
 });
 
 var CommentList = React.createClass({
-
   mixins: [ParseReact.Mixin],
-
   observe: function() {
-
-
-    var Comment = Parse.Object.extend("Comment");
-    var query = new Parse.Query(Comment);
-    query.find( {
-      success: function(object) {
-        console.log(object);
-      },
-
-      error:  function(object, error) {
-        console.log(object, error);
-      }
-    });
-
-
     return {
-      comments: (new Parse.Query(Comment))
+      comments: (new Parse.Query('Comment'))
     };
   },
 
   render: function() {
-
-    console.log(this.data);
+    console.log('data',this.data);
     var commentNodes = this.data.comments.map(function (comment) {
       return (
-        <Comment>
-          {comment.info}
-        </Comment>
+        <p>{comment.info}</p>
       );
     });
 
@@ -88,18 +39,6 @@ var CommentList = React.createClass({
     )
   }
 });
-
-
-var Comment = React.createClass({
-  render:  function() {
-    return (
-      <div className="comment">
-        <h2>Comment: </h2>
-        {this.props.children}
-      </div>
-    )
-  }
-})
 
 
 React.render(
